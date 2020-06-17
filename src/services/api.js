4b5424858f1797f3ef9f159e5fec416f01b902af
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-06-17 18:00:40 -0300

    added prettier and eslint configs/ dependencies

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 export async function getCategories() {
-  const categories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/categories'
+  );
   return categories.json();
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const categoryAndQuery = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const categoryAndQuery = await fetch(
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+  );
   return categoryAndQuery.json();
 }
