f4f82014f57f7e95c24da13e679fb11ec947301f
frederico-campello <fredfmc33@gmail.com>
2020-06-18 08:55:03 -0300

    Requisito 04

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import * as api from '../services/api';
+import Categorie from '../components/Categorie';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((categories) => {
+      this.setState({
+        categories,
+      });
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </div>
+      <>
+        <div data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </div>
+        <div>
+          {categories.map((categorie) => (
+            <Categorie key={categorie.id} categorie={categorie} />
+          ))}
+        </div>
+      </>
     );
   }
 }
