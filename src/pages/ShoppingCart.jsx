f253d54c3a09efe169d571aaa607415a8cdc064b
frederico-campello <fredfmc33@gmail.com>
2020-06-21 12:09:39 -0300

    Iniciando requisito 08

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -12,37 +12,15 @@ class ShoppingCart extends Component {
             Seu carrinho está vazio
           </h3>
         ) : (
-          carrinho.map((produto) => <h3 key={produto.id}>{produto.title}</h3>)
+          carrinho.map((produto) =>
+          <div key={produto.id}>
+            <h5 data-testid="shopping-cart-product-name">{produto.title}</h5>
+            <h6 data-testid="shopping-cart-product-quantity">{produto.available_quantity}</h6>
+          </div>
+          )
         )}
       </div>
     );
   }
 }
 export default ShoppingCart;
-
-
-
-
-
-
-// class ShoppingCart extends Component {
-//   render() {
-//     return (
-//       // condição para exibir mensagem de carrinho vazio ou exibir os produtos adicionados
-//       <div>
-//         <h1>Carrinho de compras</h1>
-//         <h3 data-testid="shopping-cart-empty-message">
-//           Seu carrinho está vazio
-//         </h3>
-//         <h5 data-testid="shopping-cart-product-name">
-//           Produto: {localStorage.getItem('Product')}
-//           {/* {title} */}
-//         </h5>
-//         <h6 data-testid="shopping-cart-product-quantity">
-//           Quantidade: {localStorage.getItem('Quantity')}
-//           {/* {available_quantity} */}
-//         </h6>
-//       </div>
-//     );
-//   }
-// }
