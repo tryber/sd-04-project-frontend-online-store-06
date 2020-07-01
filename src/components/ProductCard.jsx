2c3674773ad78ec5971e36b3a70e5b4bd88b63c0
frederico-campello <fredfmc33@gmail.com>
2020-07-01 16:11:06 -0300

    Iniciando requisito 15

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,7 +4,36 @@ import { Link } from 'react-router-dom';
 class ProductCard extends Component {
   render() {
     const { product, addProduct } = this.props;
-    const { title, thumbnail, price } = product;
+    const { title, thumbnail, price, shipping } = product;
+
+    if (shipping.free_shipping === true) {
+      return (
+        <div data-testid="product">
+          <Link
+            to={{
+              pathname: `/details/${product.id}`,
+              product,
+            }}
+            data-testid="product-detail-link"
+          >
+            <h1>{title}</h1>
+            <img src={thumbnail} title={title} alt={title} />
+          </Link>
+          <p>{price}</p>
+          <button
+            type="button"
+            data-testid="product-add-to-cart"
+            onClick={() => {
+              addProduct(product);
+            }}
+          >
+            Adicionar ao Carrinho
+          </button>
+          <div data-testid="free-shipping">FRETE GRÁTIS</div>
+        </div>
+      );
+    }
+
     return (
       <div data-testid="product">
         <Link
